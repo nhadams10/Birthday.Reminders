9d1d5ce90410c8fc6c1ef730b8800c45e0f91e28
Noah Adams <nhadams10@uchicago.edu>
2021-09-24 11:06:09 +0000

    First working prototype complete

--- birthday_book.py
+++ birthday_book.py
@@ -1,8 +1,9 @@
-birthdays = {'29.05.1970': 'Tony Stark', '18.12.1969': 'Bruce Banner'}
-
-#Need to reformat these
-#04.07.1919  Steve Rogers
-#03.12.1984  Natasha Romanoff
-#18.06.1982  Clint Barton
-#02.06.0965  Thor Odinsson
-#22.09.2021  Test User
+birthdays = {
+    '29.05.1970': 'Tony Stark', 
+    '18.12.1969': 'Bruce Banner', 
+    '04.07.1919': 'Steve Rogers', 
+    '03.12.1984': 'Natasha Romanoff', 
+    '18.06.1982': 'Clint Barton', 
+    '02.06.0965': 'Thor Odinsson', 
+    '24.09.2021': 'Test User'
+    }
--- weakest_pen.py
+++ weakest_pen.py
@@ -2,32 +2,22 @@
 import time
 from datetime import datetime
 
-
-#birthday_book is a file containing birthdays for anyone you want a reminder for
+#birthday_book is a file containing birthdays stored as a dictionary for anyone you want a reminder for
 from birthday_book import *
 
 #Eventually need to revise to account for time zones
 today = time.strftime('%d.%m.%Y')
-print(today)
-
-#A date for testing purposes
-test_date = '22.08.2021'
-print(test_date)
 
 #Empty string for storing today's birthdays
-todays_birthdays = 'Test'
+todays_birthdays = ''
 
+#For loop checkings dates against today's date
+for day, name in birthdays.items():
+    if todays_birthdays == "":
+        if day == today:
+            todays_birthdays += name
+    else:
+        if day == today:
+            todays_birthdays += ", " + name
 
-
-def birthday_checker(list):
-for line in birthday_strings_list:
-    print(line)
-        #if today in line:
-            #name = line.split('  ')[1]
-            #todays_birthdays += name
-    #if todays_birthdays == "":
-        #todays_birthdays = "No Birthdays Today"
-
-print(birthday_strings_list)
-
-birthdays.close()
+print(todays_birthdays)
