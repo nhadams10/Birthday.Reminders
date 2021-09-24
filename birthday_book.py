649513f83777547ea490a22abf989c7c8ee72a7d
Noah Adams <nhadams10@uchicago.edu>
2021-09-24 09:56:08 +0000

    Started reformatting program to keep birthdays in a dictionary

---
+++ birthday_book.py
@@ -0,0 +1,8 @@
+birthdays = {'29.05.1970': 'Tony Stark', '18.12.1969': 'Bruce Banner'}
+
+#Need to reformat these
+#04.07.1919  Steve Rogers
+#03.12.1984  Natasha Romanoff
+#18.06.1982  Clint Barton
+#02.06.0965  Thor Odinsson
+#22.09.2021  Test User
--- weakest_pen.py
+++ weakest_pen.py
@@ -2,10 +2,9 @@
 import time
 from datetime import datetime
 
-#Birthday_Book is a file containing birthdays for anyone you want a reminder for, written in the format: Day.Month.Year  First_Name Last_Name
-#Might want to update the format of this at a later time
-#Setting variable equal to file path
-Birthday_Book = '/workspace/Birthday.Reminders/Birthday_Book.txt'
+
+#birthday_book is a file containing birthdays for anyone you want a reminder for
+from birthday_book import *
 
 #Eventually need to revise to account for time zones
 today = time.strftime('%d.%m.%Y')
@@ -16,16 +15,19 @@ test_date = '22.08.2021'
 print(test_date)
 
 #Empty string for storing today's birthdays
-todays_birthdays = ''
+todays_birthdays = 'Test'
 
-def birthday_checker():
-    birthdays = open(Birthday_Book, 'r')
-    for line in birthdays:
-        if today in line:
-            name = line.split('  ')[1]
-            todays_birthdays += name
+
+
+def birthday_checker(list):
+for line in birthday_strings_list:
+    print(line)
+        #if today in line:
+            #name = line.split('  ')[1]
+            #todays_birthdays += name
     #if todays_birthdays == "":
         #todays_birthdays = "No Birthdays Today"
-    print(todays_birthdays)
 
+print(birthday_strings_list)
 
+birthdays.close()
